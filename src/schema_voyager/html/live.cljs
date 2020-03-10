73f9eff680cfb0313354dce79a66159ef6d59573
Jacob Maine <jacob.maine@gmail.com>
2020-03-10 13:32:35 -0700

    Switch page header icon

--- src/schema_voyager/html/live.cljs
+++ src/schema_voyager/html/live.cljs
@@ -46,6 +46,11 @@
     [:div.border-4.border-dashed.border-gray-200.rounded-lg.h-96]]
    "<!-- /End replace -->"]]]
 
+(def voyage-icon
+  [:svg.inline.fill-none.stroke-current.stroke-2.w-8.h-8 {:viewBox "0 0 24 24"}
+   [:title "Schema Voyage"]
+   [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M5 3v4M3 5h4M6 17v4m-2-2h4m5-16l2.286 6.857L21 12l-5.714 2.143L13 21l-2.286-6.857L5 12l5.714-2.143L13 3z"}]])
+
 (defn main-view []
   (let [route (<sub [::active-route])]
     [:div.min-w-screen.font-sans.text-gray-900
@@ -53,8 +58,8 @@
       [:div.max-w-7xl.mx-auto.py-6.px-4.sm:px-6.lg:px-8
        [:h1.text-3xl.font-bold.leading-tight.text-white
         (if (= :route/collections (:name (:data route)))
-          "Schema Voyage"
-          [:a {:href (util/href :route/collections)} "Schema Voyage"])]]]
+          voyage-icon
+          [:a {:href (util/href :route/collections)} voyage-icon])]]]
      [:main.bg-gray-200.min-h-screen
       [:div.max-w-7xl.mx-auto.py-6.sm:px-6.lg:px-8
        (when-let [view (:view (:data route))]
