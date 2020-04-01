cd76d0487367216db6fecc36249d3230dea686bc
Jacob Maine <jacob.maine@gmail.com>
2020-04-01 13:15:33 -0700

    Give file ingest CLI at least a bit of documentation

--- dev/ingest/files.clj
+++ dev/ingest/files.clj
@@ -1,4 +1,9 @@
 (ns ingest.files
+  "A CLI for ingesting one or more schema files.
+
+  ```
+  clj -A:ingest -m ingest.files <file>+
+  ```"
   (:require [schema-voyager.ingest.files :as ingest.files]
             [schema-voyager.data :as data]
             [schema-voyager.ingest.core :as ingest]
