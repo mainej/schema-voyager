b80900a71d4062a3e5418563215ebd01568a5a61
Jacob Maine <jacob.maine@gmail.com>
2020-04-07 15:31:46 -0700

    Infer tuple references

--- src/schema_voyager/ingest/datomic.clj
+++ src/schema_voyager/ingest/datomic.clj
@@ -102,6 +102,32 @@
                   (lift-ident :db/unique)))
         (remove #(excluded-attr? % exclusions)))))
 
+(def ^:private reference-rules
+  '[[(referred-attr-from-entity [?referred-e] ?referred-attr)
+     (or
+      ;; attribute
+      (and
+       [(missing? $ ?referred-e :db/ident)]
+       [?referred-e ?referred-attr])
+      ;; constant
+      (and
+       [?referred-e :db/ident]
+       [(identity ?referred-e) ?referred-attr]))]])
+
+(defn- references-from-attrs [exclusions attr-pairs]
+  (->> attr-pairs
+       (map (fn [[refers-attr referred-attr]]
+              [(lift-ident refers-attr :db/valueType)
+               (data/attribute-derive-collection (lift-ident referred-attr :db/valueType))]))
+       distinct
+       (remove (fn [[refers-attr _]]
+                 (excluded-attr? refers-attr exclusions)))
+       (group-by first)
+       (map (fn [[refers-attr grouped]]
+              [refers-attr (map second grouped)]))
+       (map (fn [[refers-attr referred-colls]]
+              (assoc refers-attr :db.schema/references referred-colls)))))
+
 (defn infer-references
   "Infer references between attributes by inspecting the things to which they
   refer.
@@ -121,33 +147,99 @@
   ([db] (infer-references db {}))
   ([db exclusions]
    (->> (d/q '[:find (pull ?refers-attr [:db/ident :db/valueType]) (pull ?referred-attr [:db/ident :db/valueType])
+               :in $ %
                :where
                [?refers-attr :db/valueType :db.type/ref]
                [_ ?refers-attr ?referred-e]
-               (or
-                ;; attribute
-                (and
-                 [(missing? $ ?referred-e :db/ident)]
-                 [?referred-e ?referred-attr])
-                ;; constant
-                (and
-                 [?referred-e :db/ident]
-                 [(identity ?referred-e) ?referred-attr]))]
+               (referred-attr-from-entity ?referred-e ?referred-attr)]
+             db reference-rules)
+        (references-from-attrs exclusions))))
+
+(defn infer-homogeneous-tuple-references
+  "Infer references from homogeneous tuple attributes by inspecting the things
+  to which they refer.
+
+  Ignores attributes that are not in-use.
+
+  NOTE: Makes a (large) assumption. Each item in the tuple could refer to
+  different types of entities. This function assumes that the first item in the
+  tuple *can* and *will* refer to all the interesting types. That means that if
+  collection-a never appears in the first item of the tuple, this will fail to
+  infer that collection-a is a reference. Of course, if your data is structured
+  that way, perhaps you would be better suited by a heterogeneous tuple.
+
+  Ignores attributes that are excluded per the `exclusions`. See
+  [[excluded-attr?]].
+
+  This may help kick start your supplemental schema, but consider running it
+  once, caching the results in a file, then maintaing it by hand."
+  ([db] (infer-homogeneous-tuple-references db {}))
+  ([db exclusions]
+   (->> (d/q '[:find (pull ?refers-attr [:db/ident :db/valueType]) (pull ?referred-attr [:db/ident :db/valueType])
+               :in $ %
+               :where [?refers-attr :db/tupleType :db.type/ref]
+               [_ ?refers-attr ?tuple]
+               [(untuple ?tuple) [?referred-e]]
+               (referred-attr-from-entity ?referred-e ?referred-attr)]
+             db reference-rules)
+        (references-from-attrs exclusions))))
+
+(defn infer-heterogeneous-tuple-references
+  "Infer references from heterogeneous tuple attributes by inspecting the things
+  to which they refer.
+
+  Ignores attributes that are not in-use.
+
+  Ignores attributes that are excluded per the `exclusions`. See
+  [[excluded-attr?]].
+
+  WARNING: This has not been tested on large databases, where it may have
+  performance impacts. This is even more true than [[infer-references]] because
+  it executes one query per tuple attribute per `:db.type/ref`. Use at your own
+  risk.
+
+  This may help kick start your supplemental schema, but consider running it
+  once, caching the results in a file, then maintaing it by hand."
+  ([db] (infer-heterogeneous-tuple-references db {}))
+  ([db exclusions]
+   (->> (d/q '[:find (pull ?tuple-attr [:db/id :db/ident :db/valueType :db/tupleTypes])
+               :where [?tuple-attr :db/tupleTypes]]
              db)
-        (keep (fn [[refers-attr referred-attr]]
-                (let [refers-attr (-> refers-attr
-                                      (lift-ident :db/valueType))]
-                  (when-not (excluded-attr? refers-attr exclusions)
-                    (let [referred-coll (-> referred-attr
-                                            (lift-ident :db/valueType)
-                                            data/attribute-derive-collection)]
-                      [refers-attr referred-coll])))))
-        distinct
-        (group-by first)
-        (map (fn [[refers-attr grouped]]
-               [refers-attr (map second grouped)]))
-        (map (fn [[refers-attr referred-colls]]
-               (assoc refers-attr :db.schema/references referred-colls))))))
+        (map first)
+        (map #(lift-ident % :db/valueType))
+        (remove #(excluded-attr? % exclusions))
+        (keep (fn [refers-attr]
+                (when-let [tuple-refs (->> refers-attr
+                                           :db/tupleTypes
+                                           (keep-indexed (fn [position kw]
+                                                           (when (= kw :db.type/ref)
+                                                             position)))
+                                           (keep (fn [position]
+                                                   (let [untuple-bindings (->> refers-attr
+                                                                               :db/tupleTypes
+                                                                               count
+                                                                               range
+                                                                               (map (fn [type-position]
+                                                                                      (if (= type-position position)
+                                                                                        '?referred-e
+                                                                                        '_)))
+                                                                               vec)
+                                                         referred-colls   (->> (d/q {:find  '[(pull ?referred-attr [:db/ident :db/valueType])]
+                                                                                     :in    '[$ % ?refers-attr]
+                                                                                     :where ['[_ ?refers-attr ?tuple]
+                                                                                             ['(untuple ?tuple) untuple-bindings]
+                                                                                             '(referred-attr-from-entity ?referred-e ?referred-attr)] }
+                                                                                    db reference-rules (:db/id refers-attr))
+                                                                               (map first)
+                                                                               (map #(lift-ident % :db/valueType))
+                                                                               (map data/attribute-derive-collection)
+                                                                               distinct)]
+                                                     (when (seq referred-colls)
+                                                       {:db.schema.tuple/position position
+                                                        :db.schema/references     referred-colls}))))
+                                           seq)]
+                  {:db/ident                   (:db/ident refers-attr)
+                   :db.schema/tuple-references tuple-refs}))))))
 
 (defn infer-deprecations
   "Infer deprecated attributes and constants, based on whether they are used.
