6e9e3cf2407cc92b95adceb04068fbf2c4d5d656
Jacob Maine <jacob.maine@gmail.com>
2020-04-15 12:04:01 -0700

    Restore visibility of lock icon on unique attributes

--- src/schema_voyager/html/util.cljs
+++ src/schema_voyager/html/util.cljs
@@ -111,7 +111,7 @@
   (pipe-list (map attr-link attributes)))
 
 (def lock-closed
-  [:svg.inline.fill-none.stroke-purle-700.stroke-2.w-4.h-4 {:viewBox "0 0 24 24"}
+  [:svg.inline.fill-none.text-purple-700.stroke-current.stroke-2.w-4.h-4 {:viewBox "0 0 24 24"}
    [:title ":db.unique/identity"]
    [:path {:stroke-linecap "round" :stroke-linejoin "round" :d "M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"}]])
 
