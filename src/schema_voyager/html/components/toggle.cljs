0a9541f147c7b585622bcc8ab50dfe1acc88189d
Jacob Maine <jacob.maine@gmail.com>
2020-04-20 16:24:18 -0700

    Speed up toggle animation

--- src/schema_voyager/html/components/toggle.cljs
+++ src/schema_voyager/html/components/toggle.cljs
@@ -10,14 +10,14 @@
                     (on-change)))})
 
 (defn span [{:keys [checked] :as props}]
-  [:span.relative.inline-block.leading-none.flex-shrink-0.h-4.w-6.border-2.border-transparent.rounded-full.transition-colors.ease-in-out.duration-200.focus:outline-none.focus:shadow-outline
+  [:span.relative.inline-block.leading-none.flex-shrink-0.h-4.w-6.border-2.border-transparent.rounded-full.transition-colors.ease-in-out.duration-150.focus:outline-none.focus:shadow-outline
    (-> props
        (dissoc :checked)
        (assoc :aria-checked (pr-str checked)
               :tabIndex     "0"
               :role         "checkbox"
               :class        (if checked :bg-teal-400 :bg-gray-200)))
-   [:span.inline-block.h-3.w-3.rounded-full.bg-white.shadow.transform.transition.ease-in-out.duration-200
+   [:span.inline-block.h-3.w-3.rounded-full.bg-white.shadow.transform.transition.ease-in-out.duration-150
     {:class       (if checked :translate-x-2 :translate-x-0)
      :aria-hidden "true"}]])
 
